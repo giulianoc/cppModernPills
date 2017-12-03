7474c922bbd7079d83182d0db008753298282866
giulianoc@catrasoftware.it <giulianoc@catrasoftware.it>
2017-12-03 16:02:48 +0100

    added more details in rvalue reference move semantics

--- 009_rvalueReference-movingSemantics/src/rvalueReference-movingSemantics.cxx
+++ 009_rvalueReference-movingSemantics/src/rvalueReference-movingSemantics.cxx
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -10,14 +11,14 @@ void printInt (int&& i) { cout << "rvalue reference: " << i << endl; };
 class BoVector {
 	int size;
 	double *arr_;	// a big array
-	
+
 	public:
 		BoVector (int size)
 		{
 			this -> size = size;
 			arr_ = new double[size];
 		}
-		
+
 		BoVector(const BoVector& rhs)	// Copy constructor (very costly deep copy)
 		{
 			size = rhs.size;
@@ -27,12 +28,12 @@ class BoVector {
 				arr_[i] = rhs.arr_[i];
 			}
 		}
-		
+
 		BoVector(BoVector&& rhs)	// Move constructor (inexpensive shallow copy)
 		{
 			size = rhs.size;
 			arr_ = rhs.arr_;
-			
+
 			rhs.arr_ = nullptr;
 		}
 
@@ -43,6 +44,26 @@ void foo (BoVector v) { };
 
 BoVector createBoVector() { return BoVector(100000); };	// creates a BoVector
 
+class Widget {
+    using DataType = vector<double>;    // using is like typedef but better
+
+    public:
+        DataType& data() &
+        {
+            return values;
+        }
+
+        DataType&& data() &&
+        {
+            return move(values);
+        }
+
+    private:
+        DataType values;
+};
+
+Widget makeWidget() { return Widget(); };
+
 int main()
 {
 	// Kind of definition
@@ -50,7 +71,7 @@ int main()
 		auto a = 5;		// a is lvalue
 		auto& b = a;		// b is lvalue reference
 		// int&& c		// c is rvalue reference
-		
+
 		printInt(a);	// call printInt (int& i) because a is lvalue
 		printInt(6);	// call printInt (int&& i) because 6 is rvalue
 	}
@@ -59,12 +80,20 @@ int main()
 	{
 		BoVector reusable = createBoVector();
 		foo(reusable);	// reusable is lvalue, it would invoke the costly Copy Constructor but it's ok we want to reuse reusable
-		
+
 		foo(createBoVector());	// createBoVector() is rvalue, it would copy the inexpensive Move Constructor
-		
+
 		// in case reusable it is not used anymore from here on, we can use
-		foo(std::move(reusable));	// it will call the Move Constructor 
+		foo(std::move(reusable));	// it will call the Move Constructor
 	}
-		
+
+	{
+        Widget w;
+
+        auto vals1 = w.data();      // call lvalue reference overloaded of data(), copy constructor is called for the vector, copy w.values into vals1
+
+        auto vals2 = makeWidget();  // call rvalue reference overloaded of data(), move constructor is called for the vector, NO copy from w.values into vals1 is done
+	}
+
 	return 0;
 }
